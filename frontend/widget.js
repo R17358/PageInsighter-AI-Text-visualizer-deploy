41ff818f703e8a795bd904dd2a5be8f03636440f
Ritesh Pandit <134190903+R17358@users.noreply.github.com>
2026-04-17 13:11:02 +0530

    Change chat button text to emoji

--- frontend/widget.js
+++ frontend/widget.js
@@ -1,7 +1,7 @@
 (function () {
   // ================= BUTTON =================
   const chatBtn = document.createElement("button");
-  chatBtn.innerText = "💬PageInsighter";
+  chatBtn.innerText = "💬";
   
   Object.assign(chatBtn.style, {
     position: "fixed",
